ad578e7cfdbb25ea015497d240d5df33942824fb
AlexHamm <hamma@purdue.edu>
2016-10-07 03:19:09 -0400

    added a few designs, made current api compatible w/ token (maybe?)

--- groupr/routes/api.js
+++ groupr/routes/api.js
@@ -14,27 +14,27 @@ var Group = require('../models/group');
  */
 var formGroupQuery = function(param) {
     var query = {
-        _id: (req.param.id == null) ? undefined : req.param.id,
-        name: (req.param.name == null) ? undefined : req.param.name,
-        creator: (req.param.creator == null) ? undefined : req.param.creator,
-        users: [(req.param.hasUser == null) ? undefined : req.param.hasUser]
+        _id: (param.id == null) ? undefined : param.id,
+        name: (param.name == null) ? undefined : param.name,
+        creator: (param.creator == null) ? undefined : param.creator,
+        users: param.user.name
     };
     return query;
 };
 var formMeetingQuery = function(param) {
     var query = {
-        _id: (req.param.id == null) ? undefined : req.param.id,
-        group: (req.param.group == null) ? undefined : req.param.group,
-        name: (req.param.name == null) ? undefined : req.param.name,
-        creator: (req.param.creator == null) ? undefined : req.param.creator,
-        date: (req.param.date == null) ? undefined : req.param.date,
-        location: (req.param.location == null) ? undefined : req.param.location,
+        _id: (param.id == null) ? undefined : param.id,
+        group: (param.group == null) ? undefined : param.group,
+        name: (param.name == null) ? undefined : param.name,
+        creator: (param.creator == null) ? undefined : param.creator,
+        date: (param.date == null) ? undefined : param.date,
+        location: (param.location == null) ? undefined : param.location,
     };
     return query;
 };
 
-router.get('/api/groups/all*', function (req, res, next) {
-    var query = formGroupQuery(req.param);
+router.get('/api/groups/all', function (req, res, next) {
+    var query = formGroupQuery(req.body);
     Group.findOne(query, function (err, group) {
         if (err)
             res.status(500).json({ message: 'Error: Database access' });
@@ -42,8 +42,8 @@ router.get('/api/groups/all*', function (req, res, next) {
             res.status(200).json({ group: group });
     });
 });
-router.get('/api/groups/calendar*', function (req, res, next) {
-    var query = formGroupQuery(req.param);
+router.get('/api/groups/calendar', function (req, res, next) {
+    var query = formGroupQuery(req.body);
     Group.findOne(query, 'calendar', function (err, group) {
         if (err)
             res.status(500).json({ message: 'Error: Database access' });
@@ -51,8 +51,8 @@ router.get('/api/groups/calendar*', function (req, res, next) {
             res.status(200).json({ calendar: group });
     });
 });
-router.get('/api/groups/id*', function (req, res, next) {
-    var query = formGroupQuery(req.param);
+router.get('/api/groups/id', function (req, res, next) {
+    var query = formGroupQuery(req.body);
     Group.findOne(query, '_id', function (err, group) {
         if (err)
             res.status(500).json({ message: 'Error: Database access' });
@@ -60,14 +60,40 @@ router.get('/api/groups/id*', function (req, res, next) {
             res.status(200).json({ calendar: group });
     });
 });
-router.get('/api/groups/meeting*', function (req, res, next) {
-    var query = formMeetingQuery(req.param);
+router.get('/api/groups/meeting', function (req, res, next) {
+    var query = formMeetingQuery(req.body);
     Meeting.findOne(query, function (err, meeting) {
         if (err)
             res.status(500).json({ message: 'Error: Database access' });
         else
             res.status(200).json({ meeting: meeting });
     });
+});
+
+// Route Protector
+router.use((req, res, next) => {
+    var token = req.body.token;
+    if (!token) {
+        token = req.query.state;
+    }
+    if (token) {
+        jwt.verify(token, conf.TOKEN_SECRET, function(err, decoded) {
+            if (err) {
+                res.status(450).json({
+                    message: 'Error: Invalid token'
+                });
+            }
+            else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    }
+    else {
+        res.status(450).json({
+            message: 'Error: Invalid token'
+        });
+    }
 });
 
 module.exports = router;
