ce958620d66279ccc0b91a3680981211e271a547
mitchholt <holt21@purdue.edu>
2016-10-23 19:57:46 -0400

    calendar file
    new files for calendar APIs

---
+++ Groupr/api_logic/api_calendar.js
@@ -0,0 +1,14 @@
+// Libraries
+
+// Models
+var User = require('../models/user');
+var Calendar = require('../models/calendar');
+var Events = require('../models/events');
+
+var calendar = new Object();\
+
+calendar.import = function() {
+
+}
+
+module.exports = calendar;
