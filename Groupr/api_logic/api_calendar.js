8e462b25b15a2e9a2a352839815f7b77ba03f940
mitchholt <holt21@purdue.edu>
2016-10-23 19:58:06 -0400

    typo

--- Groupr/api_logic/api_calendar.js
+++ Groupr/api_logic/api_calendar.js
@@ -5,7 +5,7 @@ var User = require('../models/user');
 var Calendar = require('../models/calendar');
 var Events = require('../models/events');
 
-var calendar = new Object();\
+var calendar = new Object();
 
 calendar.import = function() {
 
