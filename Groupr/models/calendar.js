7a6bd013c387d395d193d91eb1358ef6cd71b83b
mitchholt <holt21@purdue.edu>
2016-10-25 18:21:39 -0400

    calendar schema
    Note that both start time and end time will be full date/Time objects.
    We can use either to pull out just the date part.

--- Groupr/models/calendar.js
+++ Groupr/models/calendar.js
@@ -3,11 +3,13 @@ var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
     
 var CalendarSchema = new mongoose.Schema({
-    events: [
-    {
-    	 
-    }
-    ]
+    events: [{
+    	name: String,
+    	location: String,
+    	startTime: Date,
+    	endTime: Date,
+    	description: String
+    }]
 });
 
 module.exports = mongoose.model('Calendar', CalendarSchema);
