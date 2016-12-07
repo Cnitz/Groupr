2b037758dcddb85779b77bca517d5510fb93b895
Jestus <jklan1453@gmail.com>
2016-12-07 15:07:34 -0500

    Someone nuked the HomeController
    but now it's fixed

--- groupr/client/Controllers/HomeController.js
+++ groupr/client/Controllers/HomeController.js
@@ -20,7 +20,8 @@ define([
 			vm.editEvent = editEvent;
 			vm.googleAuth = googleAuth;
 			vm.printDate = printDate;
-			vm.printTimes = printTimes;
+            vm.printTimes = printTimes;
+            vm.goToTaskGroup = goToTaskGroup;
 			$scope.currentNavItem = "home";
 			$scope.myDate = new Date();
 			$scope.user = {};
@@ -33,7 +34,12 @@ define([
 			function printDate(event){
 				var newDate = new Date(event.startTime);
 				return (newDate.getMonth()+1)+'/'+newDate.getDate();
-			}
+            }
+
+            function goToTaskGroup(t) {
+                console.log(t);
+                $state.go('groupindiv', { groupID: t.group });
+            } 
 
 			function printTimes(event){
 				var newStartTime = new Date(event.startTime);
@@ -148,17 +154,38 @@ define([
 							}
 						);
 
-						// get the users groups
-						GroupServices.getGroupByUser()
-						.then(function(res) {
-							vm.groups = res.data.data;
-							}, 
-							function(res) {
-								console.log(res.data);
-								if (res.status == 450)
-									$state.go('login');
-							}
-						);
+                        // get the users groups 
+                        GroupServices.getGroupByUser()
+                            .then(function (res) {
+                                vm.groups = res.data.data;
+                            },
+                            function (res) {
+                                console.log(res.data);
+                                if (res.status == 450)
+                                    $state.go('login');
+                            }
+                            );
+                        AccountServices.getUser()
+                            .then(function (result) {
+                                var user = result.data;
+                                var data = {};
+
+
+
+                                // get the users tasks 
+                                GroupServices.getTasksByUser()
+                                    .then(function (res) {
+                                        vm.tasks = res.data;
+                                        console.log(res);
+
+                                    },
+                                    function (res) {
+                                        console.log(res.data)
+                                        if (res.status == 450)
+                                            $state.go('login');
+                                    }
+                                    );
+                            });
 					},
 					function(result) {
 						console.log(result.data);
