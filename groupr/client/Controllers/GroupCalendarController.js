28424ebc6e4b8f4a03099566aa272159a6091a7f
mitchholt <holt21@purdue.edu>
2016-12-08 19:34:10 -0500

    Doodle looking fresh

--- groupr/client/Controllers/GroupCalendarController.js
+++ groupr/client/Controllers/GroupCalendarController.js
@@ -406,9 +406,51 @@ define([
                 )
             }
 
+            function mapMonth(month) {
+                switch (month) {
+                    case 1: 
+                        return 'January';
+                    break;
+                    case 2: 
+                        return 'February';
+                    break;
+                    case 3: 
+                        return 'March';
+                    break;
+                    case 4: 
+                        return 'April';
+                    break;
+                    case 5: 
+                        return 'May';
+                    break;
+                    case 6: 
+                        return 'June';
+                    break;
+                    case 7: 
+                        return 'July';
+                    break;
+                    case 8: 
+                        return 'August';
+                    break;
+                    case 9: 
+                        return 'September';
+                    break;
+                    case 10: 
+                        return 'October';
+                    break;
+                    case 11: 
+                        return 'November';
+                    break;
+                    case 12: 
+                        return 'December';
+                    break;
+                }
+            }
+
             function printDate(event) {
                 var newDate = new Date(event.startTime);
-                return (newDate.getMonth() + 1) + '/' + newDate.getDate();
+                var month = mapMonth(newDate.getMonth() + 1);
+                return (month + ' ' + newDate.getDate() + ' From ')
             }
 
             function printTimes(event) {
